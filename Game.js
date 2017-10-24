67d88c261441394bc4c8751e781f7e51cc851bb7
meyadd <eyad.aldarra@gmail.com>
2017-10-24 20:09:28 +0200

    homework

--- Game.js
+++ Game.js
@@ -1,18 +1,41 @@
 
-function Game () {
-
-    this.board = '';
-    this.playerOne = '';
-    this.playerTwo = '';
-
+function Game (){
+	this.board = '';
+	this.playerOne = '';
+	this.playerTwo = '';
 }
 
-Game.prototype.getBoardArray = function () {
-    return this.board.trim().split("\n").map(row => row.trim());
+Game.prototype.getWinner = function (){
+	if (this.isWinner(this.playerOne))	 
+		return 1;
+	
+	if (this.isWinner(this.playerTwo))		 
+		return 2;
 };
 
-Game.prototype.getWinner = function () {
-    return 1;
-};
+Game.prototype.isWinner = function (player_symbol){
+	let res = false;
+	player_symbol = player_symbol.toLowerCase();
+	let board_str = this.board.replace(/(\r\n|\n|\r)/gm,"").toLowerCase();
+	let winning_indexes =[
+		[0,1,2],
+		[3,4,5],
+		[6,7,8],
+		[0,3,6],
+		[1,4,7],
+		[2,5,8],
+		[0,4,8],
+		[2,4,6]
+	];
+	winning_indexes.map(function(arr){
+		if(
+			board_str[arr[0]] == player_symbol 
+		 && board_str[arr[1]] == player_symbol
+		 && board_str[arr[2]] == player_symbol)
+			res = true;
+	});
+	return res;
+}
+
 
 module.exports = Game;
--- Game.spec.js
+++ Game.spec.js
@@ -1,104 +1,142 @@
 const PlayerOneSymbol = 'x';
 const PlayerTwoSymbol = 'o';
-const EmptySymbol = '-';
+const EmptyBoardSymbol = '-';
 
-const BoardEmpy = `
- ---
- ---
- ---
+const BoardEmpty = `
+---
+---
+---
 `;
 
-const BoardPlayerOneWins = `
- x--
- -x-
- --x
+const BoardPlayerOneWins1 = `
+x--
+-x-
+--x
+`;
+const BoardPlayerOneWins2 = `
+xxx
+---
+---
+`;
+const BoardPlayerOneWins3 = `
+-x-
+-x-
+-x-
+`;
+const BoardPlayerOneWins4 = `
+x-x
+-xx
+--x
 `;
 
-const BoardPlayerTwoWins = [
-    `
-        --o
-        -o-
-        o--
-    `,
-    `
-       ooo
-       ---
-       ---
-    `,
-    `
-       ---
-       ooo
-       ---
-    `,
-    `
-       ---
-       ---
-       ooo
-    `,
-    `
-       o--
-       -o-
-       --o
-    `
-];
+const BoardPlayerTwoWins1 = `
+xxo
+-ox
+o--
+`;
+const BoardPlayerTwoWins2 = `
+oxo
+oox
+oxx
+`;
+const BoardPlayerTwoWins3 = `
+-xo
+xxo
+o-o
+`;
+const BoardPlayerTwoWins4 = `
+-xx
+xox
+ooo
+`;
 
 const Game = require('./Game');
 
 
 describe("Tic Tac Toe Game", () => {
-
-    let currentGame;
-    beforeEach(() => {
-        currentGame = new Game;
+	let currentGame;
+	beforeEach(() => {
+		currentGame = new Game;
+		
+	});
+	
+it('Should create a game', () => {
+	expect(currentGame instanceof Game).toBe(true);
+	
     });
 
-    it('Should create a game', () => {
-
-        expect(currentGame instanceof Game).toBe(true);
-
+it('Should have a board', () => {
+	expect(currentGame.board).toBeDefined();
+	
     });
-
-    it('Should have a board', () => {
-
-        expect(currentGame.board).toBeDefined();
-
-    });
-
-    it('Should have one player', () => {
-
-        expect(currentGame.playerOne).toBeDefined();
-
-    });
-
-    it('Should have two players', () => {
-
-        expect(currentGame.playerTwo).toBeDefined();
-
-    });
-
-    it('Should have a function to determinate winner', () => {
-
-        expect(currentGame.getWinner).toBeDefined();
-
-    });
-
+	
+	it('Should have one player', () => {
+		expect(currentGame.playerOne).toBeDefined();
+		
+	});
+	
+	it('Should have two player', () => {
+		expect(currentGame.playerTwo).toBeDefined();
+		
+	});
+	
+	it('Should have a function to determinate winner', () => {
+		expect(currentGame.getWinner).toBeDefined();
+		
+	});
+	
+	
+	
 });
 
-describe('Game should be able to validate a sting board', () => {
-
-    let currentGame;
-    beforeEach(() => {
-        currentGame = new Game;
+describe('Game should be able to validata a sting board', () => {
+	let currentGame;
+	beforeEach(() => {
+		currentGame = new Game;
+		currentGame.playerOne = PlayerOneSymbol
+		currentGame.playerTwo = PlayerTwoSymbol
+	});
+	
+	it('Should validata player one winner given a certatain board', () => {
+		currentGame.board = BoardPlayerOneWins1;
+		expect(currentGame.getWinner()).toBe(1);
+	
     });
-
-    it('Should validate player one winner given a certain board', () => {
-        currentGame.board = BoardPlayerOneWins;
-        expect(currentGame.getWinner()).toBe(1);
+	it('Should validata player one winner given a certatain board', () => {
+		currentGame.board = BoardPlayerOneWins2;
+		expect(currentGame.getWinner()).toBe(1);
+	
     });
-
-    it('Should validate player two winner given a certain board', () => {
-        currentGame.board = BoardPlayerTwoWins;
-        expect(currentGame.getWinner()).toBe(2);
+	it('Should validata player one winner given a certatain board', () => {
+		currentGame.board = BoardPlayerOneWins3;
+		expect(currentGame.getWinner()).toBe(1);
+	
     });
-
+	it('Should validata player one winner given a certatain board', () => {
+		currentGame.board = BoardPlayerOneWins4;
+		expect(currentGame.getWinner()).toBe(1);
+	
+    });
+	
+	it('Should validata player two winner given a certatain board', () => {
+		currentGame.board = BoardPlayerTwoWins1;
+		expect(currentGame.getWinner()).toBe(2);
+	
+    });
+	it('Should validata player two winner given a certatain board', () => {
+		currentGame.board = BoardPlayerTwoWins2;
+		expect(currentGame.getWinner()).toBe(2);
+	
+    });
+	it('Should validata player two winner given a certatain board', () => {
+		currentGame.board = BoardPlayerTwoWins3;
+		expect(currentGame.getWinner()).toBe(2);
+	
+    });
+	it('Should validata player two winner given a certatain board', () => {
+		currentGame.board = BoardPlayerTwoWins4;
+		expect(currentGame.getWinner()).toBe(2);
+	
+    });
+	
 });
